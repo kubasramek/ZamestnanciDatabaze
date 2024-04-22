afb8ae193308b7021ee74337a07d7833f040e452
jakub <jakub@MSI>
2024-04-22 20:32:52 +0200

    Přidejte soubory projektu.

---
+++ ZamestnanciDatabaze/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ZamestnanciDatabaze
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
